f1a6fb22ac2755501f97148db5650c65e1d9c1a2
Tsolmon Peters <tsolp0123@gmail.com>
2020-08-01 11:44:48 -0500

    update HappyFirstDayOfGitClass.java

--- src/HappyFirstDayOfGitClass.java
+++ src/HappyFirstDayOfGitClass.java
@@ -1,5 +1,13 @@
 public class HappyFirstDayOfGitClass {
     public static void main(String[] args) {
         System.out.println("HelloAgain");
+
+
+
+
+
+
+
+        // Coding is not easy
     }
 }
